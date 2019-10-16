ccc5114fdba0735f73150f3abe5b8b9cb8ddc196
DESKTOP-1QO8RDL\123 <meow122@icloud.com>
2019-10-16 14:26:20 +0500

    Input params for components

--- app/src/app/parts/explorer/path-viewer/folder/folder.component.ts
+++ app/src/app/parts/explorer/path-viewer/folder/folder.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Folder } from 'src/app/lib/models/folder.model';
 
 @Component({
   selector: 'app-folder',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FolderComponent implements OnInit {
 
+  private _open = false;
+
+  @Input()
+  folder: Folder;
+
+  get itemsAmount(){
+    return this.folder.content.length;
+  }
+
+  get isOpen(){
+    return this._open;
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleOpen(){
+    this._open = !this._open;
   }
 
 }
